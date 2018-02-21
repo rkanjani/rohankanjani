b2c027c4e84409f9dbf61ab1c8be3ba120d7d197
Rohan Kanjani <rkanj017@uottawa.ca>
2018-02-20 23:39:51 -0500

    fixed

---
+++ files/why/app.js
@@ -0,0 +1,46 @@
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+      $(".y").typed({
+        strings: ["^1000 because of your smile.",
+                  "^1000 because of your laugh.",
+                  "^1000 because of how smart you are.",
+                  "^1000 because of how adorable you are.",
+                  "^1000 because you're my burita.",
+                  "^1000 to the moon and back x9999",
+                  "^1000 way, way too much",
+                  "^1000 I love you ^1000- wait did I say that already?",
+                  "^1000 because you're down to earth.",
+                  "^1000 because you know how to take care of yourself.",
+                  "^1000 because u get me, fam.",
+                  "^1000 - and I always will.",
+                  "^1000 more than u.",
+                  "^1000 because you know how to take care of me.",
+                  "^1000 and I hope you have an amazing Valentine's Day my boo.",
+                  "^1000 bcuz ur eyebrows r always on fleek ;)",
+                  "^1000 bcuz u watch 5 minute previews w me =)",
+                  "^1000 bcuz u got a big butt",
+                  "^1000 bcuz u tolerate me.",
+                  "^1000 because of how weirdly good you are at puzzles.",
+                  "^1000 because I can talk to you forever.",
+                  "^1000 because you really know how to listen and tell me how it is.",
+                  "^1000 cuz, I can see myself with you ^200 from like twantyfive yeaurs naow",
+                  "^1000 cuz u know most of my ideas so I gotta make sure u don't tell anyone =P",
+                  "^1000 because you really believe in me",
+                  "^1000 BCUZ YA GET ME FAM.",
+                  "^1000 - yo I swear man just believe me",
+                  "^1000 so much that I sometimes listen, only sometimes =P"],
+            typeSpeed: 0,
+            // time before typing starts
+            startDelay: 0,
+            // backspacing speed
+            backSpeed: 0,
+            // shuffle the strings
+            shuffle: true,
+            // time before backspacing
+            backDelay: 1000,
+            // loop
+            loop: true,
+
+      });
+});
